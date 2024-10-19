f3e79d9c954befc3f94f29bf9c2a84255a59dcd3
Yevhen Cherkes <ycherkes@outlook.com>
2024-10-19 22:39:45 +0200

    include DebugHelpers/SpecDebugView.cs

---
+++ src/Raffinert.Spec/DebugHelpers/SpecDebugView.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq.Expressions;
+
+namespace Raffinert.Spec.Debug;
+
+internal class SpecDebugView<T>(Spec<T> spec)
+{
+    public Expression<Func<T, bool>> Expression => spec.GetExpression();
+}
