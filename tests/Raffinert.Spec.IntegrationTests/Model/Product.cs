b805ef3c947a591a7698e90e1b33e2265ba6cd23
Yevhen Cherkes <ycherkes@outlook.com>
2024-11-03 11:19:22 +0100

    Ability to combine Specification for parent and nested items. Ex _context.Products.Where(p => bananaSpec.IsSatisfiedBy(p) && p.Categories.Any(fruitCategorySpec.IsSatisfiedBy))

--- tests/Raffinert.Spec.IntegrationTests/Model/Product.cs
+++ tests/Raffinert.Spec.IntegrationTests/Model/Product.cs
@@ -5,4 +5,13 @@ public class Product
     public int Id { get; set; }
     public required string Name { get; set; }
     public decimal Price { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
+
+public class Category
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public ICollection<Product> Products { get; set; }
 }
